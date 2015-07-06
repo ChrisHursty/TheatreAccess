196a29bd7dbd13210af9d87e7099df8e86c146bb
Chris Hurst <chris@iamchrishurst.com>
2015-07-06 16:53:05 -0400

    added and edited code for accessibility compliance

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-// A $( document ).ready() block
 
 $(document).ready(function() {
 	
@@ -6,6 +5,7 @@ $(document).ready(function() {
 	$('.bxslider').bxSlider({
 		pager: false
 	});
+	
 	
 	// Isotope Filter, Home Page
 	var $container = $('.shows'); 
@@ -16,7 +16,7 @@ $(document).ready(function() {
             easing: 'linear', 
             queue: false, 
         } 
-    }); 
+    });
   
     $('nav ul.filterBtn a').click(function(){ 
         var selector = $(this).attr('data-filter'); 
@@ -30,6 +30,7 @@ $(document).ready(function() {
         }); 
       return false; 
     });
+
 	
 	// Active/Selected State for Filters
 	var $optionSets = $('nav ul.filterBtn'), 
@@ -45,19 +46,12 @@ $(document).ready(function() {
 	   $optionSet.find('.selected').removeClass('selected'); 
 	   $this.addClass('selected');  
 	});
-
-	// Reset Filters
-	$(".isotope-reset").click(function(){
-		$("nav ul.filterBtn a").isotope({
-			filter: '*'
-		});
-	});
 	
 	
 	// Increase/Decrease Font Size
 	$('#incfont').click(function() {
 		curSize = parseInt($('#content').css('font-size')) + 2;
-		if (curSize <= 20)
+		if (curSize <= 30)
 			$('#content').css('font-size', curSize);
 	});
 	$('#decfont').click(function() {
@@ -66,10 +60,72 @@ $(document).ready(function() {
 			$('#content').css('font-size', curSize);
 	});
 	
-	$(function() {
-		$( "#datepicker" ).datepicker();
-	  })
 	
+	// Date Range Picker
+	$('input[name="daterange"]').daterangepicker();
+	
+	
+	// Single Date Picker
+	$('input[name="singledate"]').daterangepicker({
+		singleDatePicker: true,
+		showDropdowns: true
+	});
+
+	
+	// All Options
+	$('#reportrange span').html(moment().subtract(29, 'days').format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
+ 
+    $('#reportrange').daterangepicker({
+        format: 'MM/DD/YYYY',
+        startDate: moment().subtract(29, 'days'),
+        endDate: moment(),
+        minDate: '01/01/2012',
+        maxDate: '12/31/2015',
+        dateLimit: { days: 60 },
+        showDropdowns: true,
+        showWeekNumbers: true,
+        timePicker: false,
+        timePickerIncrement: 1,
+        timePicker12Hour: true,
+        ranges: {
+           'Today': [moment(), moment()],
+           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+           'This Month': [moment().startOf('month'), moment().endOf('month')],
+           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        opens: 'left',
+        drops: 'down',
+        buttonClasses: ['btn', 'btn-sm'],
+        applyClass: 'btn-primary',
+        cancelClass: 'btn-default',
+        separator: ' to ',
+        locale: {
+            applyLabel: 'Submit',
+            cancelLabel: 'Cancel',
+            fromLabel: 'From',
+            toLabel: 'To',
+            customRangeLabel: 'Custom',
+            daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr','Sa'],
+            monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            firstDay: 1
+        }
+    }, function(start, end, label) {
+        console.log(start.toISOString(), end.toISOString(), label);
+        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    });
+	
+	
+	// Display Today's Date
+	Date.prototype.toDateInputValue = (function() {
+		var local = new Date(this);
+		local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+		return local.toJSON().slice(0,10);
+	});
+	
+	$('#Date').val(new Date().toDateInputValue());
+	//$('#Date').html(moment().format('MMMM D, YYYY') );
 });
 
 
